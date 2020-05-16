85538b096db6e5a02f18ceabd15ada0e2e75b357
Igor Talankin <igortalankin@MacBook-Pro-Igor.local>
2020-05-17 00:43:38 +0300

    file logging

--- cfg.py
+++ cfg.py
@@ -1,4 +1,5 @@
 settings = {
+    
     "consumer": {
         "host": '10.31.120.59',  # required
         "username": 'mpx_siem',  # required
@@ -10,12 +11,14 @@ settings = {
         "ioc_fields": ['src.ip','dst.ip','src.host','dst.host','dst.port','object.path','object.hash','datafield1','recv_ipv4','event_src.host','event_src.title'],  # optional
         "filter": [{'field': 'event_src.title', 'operator': 'ne', 'value': 'cybertrace'}]  # optional
     },
+
     "sender": {
         "host": '10.31.120.59',  # required
         "port": 9999,  # required
         "timeout": 30,  # required
         "start_flag": 'X-KF-SendFinishedEvent'  # required
     },
+
     "monitoring": {
         "timeout": 60  # optional
     },
